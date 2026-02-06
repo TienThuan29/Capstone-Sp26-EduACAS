180d559b4a1bf4a0eb618709ebe789be816c3092
TienThuan29 <nguyentienthuan9595@gmail.com>
2026-02-06 09:03:46 +0700

    update: update ProgrammingLanguage class, add: Compiler class

--- servers/Services/AcasService/Models/ProgrammingLanguage.cs
+++ servers/Services/AcasService/Models/ProgrammingLanguage.cs
@@ -8,16 +8,38 @@ public class ProgrammingLanguage
     public string Id { get; set; } = string.Empty;
     
     [Required]
-    public string LanguageName { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    
+    public string Monaco { get; set; } = string.Empty;
     
     [Required]
-    public string Key { get; set; } = string.Empty;
-    
-    public string LanguageVersion { get; set; } = string.Empty;
+    public List<string> Extensions { get; set; } = new List<string>();
+
+    private List<string> Alias { get; set; } = new List<string>();
+
+    public string LogoFileUrl { get; set; } = string.Empty;
+
+    public string Formatter { get; set; } = string.Empty;
+
+    public string DigitSeparator { get; set; } = string.Empty;
+
+    public List<Compiler> Compilers { get; set; } = new List<Compiler>();
     
     public bool IsEnable { get; set; }
     
     public DateTime CreatedDate { get; set; }
     
     public DateTime UpdatedDate { get; set; }
+}
+
+
+public class Compiler
+{
+    public string Id { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Group { get; set; } = string.Empty;
+
+    public List<string> StdVersions { get; set; } = new List<string>();
 }
