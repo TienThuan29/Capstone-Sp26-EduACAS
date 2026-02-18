655349871dc29f433ba836f74362a3cd0023a53a
TienThuan29 <nguyentienthuan9595@gmail.com>
2026-02-18 12:09:28 +0700

    update: DiscussionIssue and Comment entity

--- servers/Services/AcasService/Models/DiscussionIssue.cs
+++ servers/Services/AcasService/Models/DiscussionIssue.cs
@@ -6,12 +6,49 @@ public class DiscussionIssue
 {
     [Key]
     public string Id { get; set; } = string.Empty;
-    
+
     [Required]
     public string ClassroomId { get; set; } = string.Empty;
-    
+
     [Required]
     public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string AuthorId { get; set; } = string.Empty;
+
+    [Required]
+    public string Content { get; set; } = string.Empty; // markdown
+
+    public string[] Attachments { get; set; } = Array.Empty<string>();
+
+    public string RefProblemId { get; set; } = string.Empty;
+
+    public DiscussionIssueStatus Status { get; set; }
+
+    public int ViewCount { get; set; }
+
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+}
+
+public enum DiscussionIssueStatus
+{
+    Open,
+    Close
+}
+
+public class Comment
+{
+    [Key]
+    public string Id { get; set; } = string.Empty;
+    
+    [Required]
+    public string IssueId { get; set; } = string.Empty;
     
     [Required]
     public string AuthorId { get; set; } = string.Empty;
@@ -19,9 +56,11 @@ public class DiscussionIssue
     [Required]
     public string Content { get; set; } = string.Empty; // markdown content
     
-    public string[] ImagesName { get; set; } = Array.Empty<string>();
-    
-    public string[] FilesName { get; set; } = Array.Empty<string>();
+    public string[] Attachments { get; set; } = Array.Empty<string>();
+
+    public int UpVoteCount { get; set; }
+
+    public List<Comment> Replies { get; set; } = new List<Comment>();
     
     public bool IsDeleted { get; set; }
     
