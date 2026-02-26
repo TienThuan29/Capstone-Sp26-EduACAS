e33604b7b8a9b910e2a61a9e699bb1fd153376b3
TienThuan29 <nguyentienthuan9595@gmail.com>
2026-02-26 08:30:00 +0700

    update: class name for testcases evaluation

---
+++ servers/Services/AcasService/Application/Commands/Submission/ExecutionCommand.cs
@@ -0,0 +1,37 @@
+using AcasService.Application.ResponseDTOs.External;
+using AcasService.Web.Requests;
+
+namespace AcasService.Application.Commands.Submission;
+
+public interface IExecutionCommand
+{
+
+    Task<CompilationResult> ExecuteCustomTestcaseAsync(string compilerId, CompileRequest compileRequest, string lang);
+
+    Task<List<TestResultResponse>> ExecuteTestcasesAsync(string compilerId, RumBatchRequest runBatchRequest, string lang);
+}
+
+public class ExecutionCommand : IExecutionCommand
+{
+      private readonly ITestcaseEvaluator _testcaseEvaluator;
+
+      private readonly ILogger<ExecutionCommand> _logger;
+
+      public ExecutionCommand(ITestcaseEvaluator testcaseEvaluator, ILogger<ExecutionCommand> logger)
+      {
+            _testcaseEvaluator = testcaseEvaluator;
+            _logger = logger;
+      }
+
+      public async Task<CompilationResult> ExecuteCustomTestcaseAsync(string compilerId, CompileRequest compileRequest, string lang)
+      {
+            var result = await _testcaseEvaluator.ExecuteCustomTestcaseAsync(compilerId, compileRequest, lang);
+            return result;
+      }
+
+      public async Task<List<TestResultResponse>> ExecuteTestcasesAsync(string compilerId, RumBatchRequest runBatchRequest, string lang)
+      {
+            var result = await _testcaseEvaluator.ExecuteTestcasesAsync(compilerId, runBatchRequest, lang);
+            return result;
+      }
+}
